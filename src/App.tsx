88f90fef9ce997b11831ccb51d7fba2bc1e7e361
ishtiaq ali khan <k.ishtiaq0001@gmail.com>
2024-07-13 11:15:31 +0530

    style: Card Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Card from "./components/card/Card";
+
 function App() {
 
   return (
-    <>Boiler Plate</>
+    <Card title={"sadf"} body={"asdfasdfas dfasdf asdf"} onClose={function (): void {
+      throw new Error("Function not implemented.");
+    } } />
   )
 }
 
