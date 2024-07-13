8adabcab48d85768cdc2ed43d444a46fa281ddcf
ishtiaq ali khan <k.ishtiaq0001@gmail.com>
2024-07-13 10:53:50 +0530

    style: mediaQueries, colors for Post Cards

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './assets/styles/reset.scss';
-
+import './assets/styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
