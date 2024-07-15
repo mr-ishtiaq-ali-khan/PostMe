e82975a5946386416f9f425230993d437df3dd25
ishtiaq ali khan <k.ishtiaq0001@gmail.com>
2024-07-15 06:46:29 +0530

    style: icons added

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,5 +1,6 @@
 import "../../assets/styles/card.scss";
 import { CardType } from "../../types/card.type";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 function Card({title, body, onClose = () => {}}: CardType) {
 
@@ -7,7 +8,9 @@ function Card({title, body, onClose = () => {}}: CardType) {
         <div className="card">
             <div className="cardTitle">
                 <span>{title}</span>
-                <button onClick={onClose}>x</button>
+                <button onClick={onClose}>
+                    <DeleteForeverIcon />
+                </button>
             </div>
             <div className="cardBody">
                 {body}
--- src/components/modal/ModalPopup.tsx
+++ src/components/modal/ModalPopup.tsx
@@ -1,12 +1,13 @@
 import "../../assets/styles/modal.scss";
 import { ModalType } from "../../types/modal.type";
+import CloseIcon from '@mui/icons-material/Close';
 
 function ModalPopup({title, body, closeModal}: ModalType) {
     return (
         <div className="modalPopup">
             <div className="modalHeader">
                 <div className="modalHeaderText">{title}</div>
-                <div onClick={closeModal} className="closeModal">x</div>
+                <div onClick={closeModal} className="closeModal"><CloseIcon /></div>
             </div>
             <div className="modalBody">{ body }</div>
         </div>
